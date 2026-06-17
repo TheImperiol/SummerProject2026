b5fb3a332a18d56a63803cc76375d271fc22ced3
TheImperiol <147831895+TheImperiol@users.noreply.github.com>
2026-06-17 10:15:10 +0100

    Moved structure code to its' own control.

--- SummerProject2026Java/src/ApplicationFX/MainSummerApp.java
+++ SummerProject2026Java/src/ApplicationFX/MainSummerApp.java
@@ -2,16 +2,9 @@ package ApplicationFX;
 
 import javafx.application.*;
 import javafx.scene.control.*;
-import javafx.geometry.Insets;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
-import javafx.scene.layout.RowConstraints;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 public class MainSummerApp extends Application{
    
@@ -19,68 +12,17 @@ public class MainSummerApp extends Application{
     @Override
     public void start(Stage stage) throws Exception {
         VBox vBox = new VBox();
-        GridPane gpMain = new GridPane();
-        GridPane gpSidebar = new GridPane();
-        GridPane gpSidebarContent = new GridPane();
-        GridPane gpEmulator = new GridPane();
-        GridPane gpSettings = new GridPane();
-        Separator sep = new Separator();
-
-        gpSettings.setGridLinesVisible(true);
-        gpMain.setGridLinesVisible(true);
-        gpEmulator.setGridLinesVisible(true);
-        gpSidebar.setGridLinesVisible(true);
-        gpSidebarContent.setGridLinesVisible(true);
-
-        ColumnConstraints mainColumnSidebar = new ColumnConstraints();
-        mainColumnSidebar.setPercentWidth(20);
-        ColumnConstraints mainColumnEmulator = new ColumnConstraints();
-        mainColumnEmulator.setPercentWidth(80);
-        ColumnConstraints sidebarColumnDevices = new ColumnConstraints();
-        sidebarColumnDevices.setPercentWidth(33);
-        ColumnConstraints sidebarColumnComponents = new ColumnConstraints();
-        sidebarColumnComponents.setPercentWidth(33);
-        ColumnConstraints sidebarColumnScripts = new ColumnConstraints();
-        sidebarColumnScripts.setPercentWidth(34);
-        ColumnConstraints sidebarColumnFilling = new ColumnConstraints();
-        sidebarColumnFilling.setPercentWidth(100);
         
-        RowConstraints mainRowSettings = new RowConstraints();
-        mainRowSettings.setPercentHeight(5);
-        RowConstraints mainRowContents = new RowConstraints();
-        mainRowContents.setPercentHeight(80);
-        RowConstraints mainRowTerminal = new RowConstraints();
-        mainRowTerminal.setPercentHeight(20);
-        RowConstraints sidebarRowTabs = new RowConstraints();
-        sidebarRowTabs.setPercentHeight(5);
-        RowConstraints sidebarRowContent = new RowConstraints();
-        sidebarRowContent.setPercentHeight(95);
-        RowConstraints sidebarRowFilling = new RowConstraints();
-        sidebarRowFilling.setPercentHeight(100);
+        Structure frontend = new Structure();
+        Separator sep = new Separator();
+        VBox.setVgrow(frontend.gpAppTopBar, Priority.ALWAYS );
 
-        gpSettings.getRowConstraints().addAll(mainRowSettings, sidebarRowContent);
-        gpSettings.getColumnConstraints().add(sidebarColumnFilling);
-        gpMain.getColumnConstraints().addAll(mainColumnSidebar, mainColumnEmulator);
-        gpMain.getRowConstraints().addAll(sidebarRowFilling);
-        gpSidebarContent.getColumnConstraints().addAll(sidebarColumnDevices, sidebarColumnComponents, sidebarColumnScripts);
-        gpSidebarContent.getRowConstraints().add(sidebarRowFilling);
-        gpEmulator.getColumnConstraints().add(sidebarColumnFilling);
-        gpEmulator.getRowConstraints().addAll(mainRowContents,mainRowTerminal);
-        gpSidebar.getColumnConstraints().add(sidebarColumnFilling);
-        gpSidebar.getRowConstraints().addAll(mainRowSettings);
 
-        VBox.setVgrow(gpSettings, Priority.ALWAYS );
+        //ScriptCard card = new ScriptCard(0.987f, 8.78f);
 
-        gpSidebar.add(gpSidebarContent,0,0);
-        gpMain.add(gpSidebar,0,0);
-        gpMain.add(gpEmulator, 1, 0);
-        gpSettings.add(gpMain,0,1);
+        //gpEmulator.add(card,0,0);
 
-        ScriptCard card = new ScriptCard(0.987f, 8.78f);
-
-        gpEmulator.add(card,0,0);
-
-        vBox.getChildren().addAll(gpSettings,sep);
+        vBox.getChildren().addAll(frontend.gpAppTopBar,sep);
 
         Scene mainScene = new Scene(vBox);
         stage.setScene(mainScene);
---
+++ SummerProject2026Java/src/ApplicationFX/Structure.java
@@ -0,0 +1,61 @@
+package ApplicationFX;
+
+import javafx.scene.layout.*;
+
+public class Structure extends GridPane {
+    GridPane gpAppTopBar;
+    public Structure(){
+        GridPane gpMain = new GridPane();
+        GridPane gpSidebar = new GridPane();
+        GridPane gpSidebarContent = new GridPane();
+        GridPane gpEmulator = new GridPane();
+        gpAppTopBar = new GridPane();
+
+        gpAppTopBar.setGridLinesVisible(true);
+        gpMain.setGridLinesVisible(true);
+        gpEmulator.setGridLinesVisible(true);
+        gpSidebar.setGridLinesVisible(true);
+        gpSidebarContent.setGridLinesVisible(true);
+
+        ColumnConstraints mainColumnSidebar = new ColumnConstraints();
+        mainColumnSidebar.setPercentWidth(20);
+        ColumnConstraints mainColumnEmulator = new ColumnConstraints();
+        mainColumnEmulator.setPercentWidth(80);
+        ColumnConstraints sidebarColumnDevices = new ColumnConstraints();
+        sidebarColumnDevices.setPercentWidth(33);
+        ColumnConstraints sidebarColumnComponents = new ColumnConstraints();
+        sidebarColumnComponents.setPercentWidth(33);
+        ColumnConstraints sidebarColumnScripts = new ColumnConstraints();
+        sidebarColumnScripts.setPercentWidth(34);
+        ColumnConstraints sidebarColumnFilling = new ColumnConstraints();
+        sidebarColumnFilling.setPercentWidth(100);
+        
+        RowConstraints mainRowSettings = new RowConstraints();
+        mainRowSettings.setPercentHeight(5);
+        RowConstraints mainRowContents = new RowConstraints();
+        mainRowContents.setPercentHeight(80);
+        RowConstraints mainRowTerminal = new RowConstraints();
+        mainRowTerminal.setPercentHeight(20);
+        RowConstraints sidebarRowTabs = new RowConstraints();
+        sidebarRowTabs.setPercentHeight(5);
+        RowConstraints sidebarRowContent = new RowConstraints();
+        sidebarRowContent.setPercentHeight(95);
+        RowConstraints sidebarRowFilling = new RowConstraints();
+        sidebarRowFilling.setPercentHeight(100);
+
+        gpAppTopBar.getRowConstraints().addAll(mainRowSettings, sidebarRowContent);
+        gpAppTopBar.getColumnConstraints().add(sidebarColumnFilling);
+        gpMain.getColumnConstraints().addAll(mainColumnSidebar, mainColumnEmulator);
+        gpMain.getRowConstraints().addAll(sidebarRowFilling);
+        gpSidebarContent.getColumnConstraints().addAll(sidebarColumnDevices, sidebarColumnComponents, sidebarColumnScripts);
+        gpSidebarContent.getRowConstraints().add(sidebarRowFilling);
+        gpEmulator.getColumnConstraints().add(sidebarColumnFilling);
+        gpEmulator.getRowConstraints().addAll(mainRowContents,mainRowTerminal);
+        gpSidebar.getColumnConstraints().add(sidebarColumnFilling);
+        gpSidebar.getRowConstraints().addAll(mainRowSettings);
+        gpSidebar.add(gpSidebarContent,0,0);
+        gpMain.add(gpSidebar,0,0);
+        gpMain.add(gpEmulator, 1, 0);
+        gpAppTopBar.add(gpMain,0,1);
+    }
+}
