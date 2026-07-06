7f36ee7c31d7eba3e85bfbb39b4f02cf09de86a9
TheImperiol <147831895+TheImperiol@users.noreply.github.com>
2026-07-06 11:31:42 +0100

    Added testing functionality to MenuItems for File MenuButton. Next shall be to add each Item's intended functionality.

--- SummerProject2026Java/app/src/main/java/ApplicationFX/FileMenu.java
+++ SummerProject2026Java/app/src/main/java/ApplicationFX/FileMenu.java
@@ -1,15 +1,40 @@
 package ApplicationFX;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 
 public class FileMenu extends MenuButton {
     public FileMenu(){
         setText("File");
+        MenuItem openProject = new MenuItem("Open Project");
+        MenuItem newProject = new MenuItem("New Project");
+        MenuItem saveProject = new MenuItem("Save Project");
+
+
+        openProject.setOnAction(new EventHandler<ActionEvent>(){
+            @Override public void handle(ActionEvent event){
+                System.out.println("Opening");
+            }
+        });
+
+        newProject.setOnAction(new EventHandler<ActionEvent>(){
+            @Override public void handle(ActionEvent event){
+                System.out.println("Creating new");
+            }
+        });
+
+        saveProject.setOnAction(new EventHandler<ActionEvent>(){
+            @Override public void handle(ActionEvent event){
+                System.out.println("Saving");
+            }
+        });
+
         getItems().addAll(
-            new MenuItem("Open Project"),
-            new MenuItem("Save Project"),
-            new MenuItem("New Project")
+            openProject,
+            newProject,
+            saveProject
         );
     }
 }
