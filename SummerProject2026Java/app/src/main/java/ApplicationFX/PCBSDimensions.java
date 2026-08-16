7298ddf4ca458aa4970e83ad0387bce45b152c6a
TheImperiol <147831895+TheImperiol@users.noreply.github.com>
2026-08-16 10:21:02 +0100

    Forced only numeric inputs to PCB dminesions.

--- SummerProject2026Java/app/src/main/java/ApplicationFX/PCBSDimensions.java
+++ SummerProject2026Java/app/src/main/java/ApplicationFX/PCBSDimensions.java
@@ -1,5 +1,7 @@
 package ApplicationFX;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
@@ -9,7 +11,23 @@ public class PCBSDimensions extends VBox {
     public PCBSDimensions(){
         PCBX.setPromptText("PCB X");
         PCBY.setPromptText("PCB Y");
-        this.setPrefSize(USE_PREF_SIZE, USE_PREF_SIZE);
+        //this.setPrefSize(USE_PREF_SIZE, USE_PREF_SIZE);
         this.getChildren().addAll(PCBX,PCBY);
+
+        PCBX.textProperty().addListener(new ChangeListener<String>() {
+            @Override public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if (!newValue.matches("\\d*")) {
+                    PCBX.setText(newValue.replaceAll("[^\\d]", ""));
+                }
+            }
+        });
+        PCBY.textProperty().addListener(new ChangeListener<String>() {
+            @Override public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if (!newValue.matches("\\d*")) {
+                    PCBY.setText(newValue.replaceAll("[^\\d]", ""));
+                }
+            }
+        });
+        
     }
 }
