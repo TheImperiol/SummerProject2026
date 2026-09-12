33ca75da6aa01ab287926a1a802109584adfc742
TheImperiol <147831895+TheImperiol@users.noreply.github.com>
2026-09-12 10:11:00 +0100

    Changing up theory of tracks so a Linked List can be formed. Need to make tracks purely decorative and each component can hold the next.

--- SummerProject2026Java/app/src/main/java/ApplicationFX/DraggableCard.java
+++ SummerProject2026Java/app/src/main/java/ApplicationFX/DraggableCard.java
@@ -6,7 +6,6 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Base64;
-
 import javafx.event.EventHandler;
 import javafx.scene.control.Control;
 import javafx.scene.input.*;
@@ -19,6 +18,9 @@ public abstract class DraggableCard extends GeneralCard implements Serializable{
     public float GetX(){
         return position[0];
     }
+    private double[] drawnTrackStart = new double[2];
+
+    public void setTrackStart(double x, double y) {drawnTrackStart[0] = x; drawnTrackStart[1] = y;}
 
     public DraggableCard GetCard(){
         return this;
@@ -36,13 +38,15 @@ public abstract class DraggableCard extends GeneralCard implements Serializable{
         position[1] = y;
     }
 
+
+    private DraggableCard getSelf(){return this;}
     
+    private Track currentTrack;
 
     public DraggableCard(float x, float y){
         super();
         SetX(x);
         SetY(y);
-
         
 
         this.setOnDragDetected(new EventHandler<MouseEvent>() {
@@ -67,6 +71,53 @@ public abstract class DraggableCard extends GeneralCard implements Serializable{
             }
         });
 
+        this.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent event){
+                if(MainSummerApp.app.GetDrawingMode() == true){
+                    setTrackStart(event.getX(),event.getY());
+                }
+                event.consume();
+            }
+        });
         
+        this.setOnDragDetected(event ->{startFullDrag(); event.consume();});
+
+        this.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent event){
+                if(MainSummerApp.app.GetDrawingMode() == true){
+                    double endX = Math.clamp(event.getX(), 0,getSelf().getPrefWidth());
+                    double endY = Math.clamp(event.getY(), 0, getSelf().getPrefHeight());
+                    
+                    if(currentTrack == null){
+                        
+                        currentTrack = new Track(
+                            drawnTrackStart[0],
+                            drawnTrackStart[1],
+                            endX,
+                            endY,
+                            MainSummerApp.app.frontend.emulatorWindow.pcb
+                        );
+                        MainSummerApp.app.frontend.emulatorWindow.pcb.getChildren().add(currentTrack);
+
+                    }
+                    else{
+                        currentTrack.UpdateEndPoint(endX, endY);
+                    }
+                }
+                //event.consume();
+            }
+        });
+
+        this.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent event){
+                if(MainSummerApp.app.GetDrawingMode() == true){
+                    if(currentTrack != null){
+                        MainSummerApp.app.frontend.emulatorWindow.pcb.AddTrack(currentTrack);
+                        currentTrack = null;
+                        drawnTrackStart = new double[2];
+                    }
+                }
+            }
+        });
     }
 }
--- SummerProject2026Java/app/src/main/java/ApplicationFX/PCB.java
+++ SummerProject2026Java/app/src/main/java/ApplicationFX/PCB.java
@@ -8,9 +8,7 @@ import javafx.scene.layout.Pane;
 
 public class PCB extends Pane{
 
-    private double[] drawnTrackStart = new double[2];
-
-    public void setTrackStart(double x, double y) {drawnTrackStart[0] = x; drawnTrackStart[1] = y;}
+  
 
     public void SetPCBSize(double _width, double _height){
         this.setPrefSize(_width, _height);
@@ -20,60 +18,13 @@ public class PCB extends Pane{
 
     private ArrayList<Track> tracks = new ArrayList<Track>();
 
-    private Track currentTrack;
+    public void AddTrack(Track track){
+        tracks.add(track);
+    }
 
     public PCB (){
         this.setStyle("-fx-background-color: #347d1c");
         this.setPrefSize(100.0,100.0);
-
-        this.setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override public void handle(MouseEvent event){
-                if(MainSummerApp.app.GetDrawingMode() == true){
-                    setTrackStart(event.getX(),event.getY());
-                }
-                event.consume();
-            }
-        });
-        
-        this.setOnDragDetected(event ->{startFullDrag(); event.consume();});
-
-        this.setOnMouseDragged(new EventHandler<MouseEvent>() {
-            @Override public void handle(MouseEvent event){
-                if(MainSummerApp.app.GetDrawingMode() == true){
-                    double endX = Math.clamp(event.getX(), 0,getSelf().getPrefWidth());
-                    double endY = Math.clamp(event.getY(), 0, getSelf().getPrefHeight());
-                    
-                    if(currentTrack == null){
-                        
-                        currentTrack = new Track(
-                            drawnTrackStart[0],
-                            drawnTrackStart[1],
-                            endX,
-                            endY,
-                            getSelf()
-                        );
-                        getSelf().getChildren().add(currentTrack);
-
-                    }
-                    else{
-                        currentTrack.UpdateEndPoint(endX, endY);
-                    }
-                }
-                //event.consume();
-            }
-        });
-
-        this.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override public void handle(MouseEvent event){
-                if(MainSummerApp.app.GetDrawingMode() == true){
-                    if(currentTrack != null){
-                        tracks.add(currentTrack);
-                        currentTrack = null;
-                        drawnTrackStart = new double[2];
-                    }
-                }
-            }
-        });
-        
+ 
     }
 }
